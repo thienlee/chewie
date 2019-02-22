330da9461f285d2e2c1a15bed088297a3b857c38
HaNguyen <ha.nguyendinh04@gmail.com>
2019-02-22 11:12:42 +0700

    add android files

---
+++ android/src/main/java/com/example/chewie/ChewieVideoPlugin.java
@@ -0,0 +1,20 @@
+package com.example.chewie;
+
+import io.flutter.plugin.common.MethodCall;
+import io.flutter.plugin.common.MethodChannel;
+import io.flutter.plugin.common.MethodChannel.MethodCallHandler;
+import io.flutter.plugin.common.MethodChannel.Result;
+import io.flutter.plugin.common.PluginRegistry.Registrar;
+
+/** ChewieVideoPlugin */
+public class ChewieVideoPlugin implements MethodCallHandler {
+  /** Plugin registration. */
+  public static void registerWith(Registrar registrar) {
+
+  }
+
+  @Override
+  public void onMethodCall(MethodCall call, Result result) {
+    result.notImplemented();
+  }
+}
